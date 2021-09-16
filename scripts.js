5d3c7c92bcf6922a2d0f1c83af0accafe3062545
Rob <klassen.rob@gmail.com>
2021-09-15 22:16:09 -0400

    slight modification

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
-// console.log("hello");
+// ==================================================
+// NOTES FOR DAVID NOTES FOR DAVID NOTES FOR DAVID 
+// ==================================================
 
-// click the images and they enlarge in a popout, with a faded div background.
+// HELLO!  THANK YOU FOR THE FEEDBACK ON PROJECT 1, 
 
-// detect click on image
-// build a div with fixed position, that contains the image that was clicked on
-//  
-// build a div with fixed position that blurs or fades the background a bit
-//
+//  NEW CODE IS IN THIS SCRIPTS FILE, AND THE NEW PARTIAL CALLED '_JStyle.scss' SO CHECK THERE FOR ANY CSS CODE.   
 
+// SO I ADDED A FEATURE THAT LETS THE USER CLICK ANY IMAGE AND HAVE IT POP OUT TO TAKE UP THE FULL SCREEN, WITH A FADED BACKGROUND AND SOME OTHER SIMPLE CSS FLAIR.
 
-// const getCurrentSource = function (img){
-//     return img.currentSrc
-// }
+// I FELT LIKE THAT WASN'T REALLY ENOUGH OF A FEATURE SO I ALSO ADDED A FEATURE TO THE GALLERY WHERE IF YOU MOUSE OVER THE GALLERY IMAGES (AT DESKTOP SIZE) IT WILL MAKE THE ONE YOU MOUSEOVER SLIGHTLY LARGER.  I KIND OF THREW THIS TOGETHER QUICKLY ON WEDNESDAY NIGHT SO IT ISN'T QUITE AS GOOD LOOKING AS I WANT BUT IT'S JUST CSS TO ADJUST AT THIS POINT, THE JS SEEMS TO WORK WELL.  
 
-// const enlargeImage = function(){
-//     console.log("triggered enlarge function");
-// }
+// MY JS ASSIGNMENT IS NOT RESPONSIVE, I AM VERY NEW TO VANILLA-JS AND THIS HAS BEEN A HUGE LEARNING EXPERIENCE, BUT I THINK I DID OKAY AT MAKING IT EFFICIENT-ISH... 
+
+// ==================================================
+// NOTES FOR DAVID NOTES FOR DAVID NOTES FOR DAVID 
+// ==================================================
 
 const app = {}
 
