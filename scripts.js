68cc3a7790777efa048bcd68d33e918893e305ad
Rob <klassen.rob@gmail.com>
2021-09-15 22:10:50 -0400

    JS Assignment Done

--- scripts.js
+++ scripts.js
@@ -17,21 +17,51 @@
 //     console.log("triggered enlarge function");
 // }
 
-let overlayImg = document.querySelectorAll('img');
-const body = document.querySelector('body');
-const newOverlay = document.createElement('div');
+const app = {}
 
-for (i=0; i<overlayImg.length; i++){
-    let image = overlayImg[i];
-    
-    overlayImg[i].addEventListener('click', function(){
-        let imageSource = image.currentSrc;
-        newOverlay.innerHTML = `<img src="${imageSource}">` 
-        newOverlay.classList.add("overlayJSClass");
-        body.appendChild(newOverlay);
-    });
-    newOverlay.addEventListener('click', function(){
-        newOverlay.innerHTML = ``;
-        this.classList.remove("overlayJSClass");
-    });
+app.imageOverlay = function(){
+    let overlayImg = document.querySelectorAll('img');
+    const body = document.querySelector('body');
+    const newOverlay = document.createElement('div');
+
+    for (i=0; i<overlayImg.length; i++){
+        let image = overlayImg[i];
+
+        overlayImg[i].addEventListener('click', function(){
+            let imageSource = image.currentSrc;
+            newOverlay.innerHTML = `<img src="${imageSource}">` 
+            newOverlay.classList.add("overlayJSClass");
+            body.appendChild(newOverlay);
+        });
+
+        newOverlay.addEventListener('click', function(){
+            newOverlay.innerHTML = ``;
+            this.classList.remove("overlayJSClass");
+        });
+    }    
 }
+
+app.galImageEnlarge = function(){
+    let galParent = document.querySelector(".galleryContainer");
+    let galImgBox = document.querySelectorAll('.mainGalImgBox');
+
+    for (i=0; i<galImgBox.length; i++){
+        let image = galImgBox[i];
+
+        image.addEventListener('mouseover', function(){
+            galParent.classList.add("mouseoverJSClassParent");
+            this.classList.add("mouseoverJSClassChild");
+        });
+        image.addEventListener('mouseout', function(){
+            galParent.classList.remove("mouseoverJSClassParent");
+            this.classList.remove("mouseoverJSClassChild");
+        });
+    }
+}
+
+app.init = function(){
+    app.imageOverlay();
+    app.galImageEnlarge();
+}
+
+app.init();
